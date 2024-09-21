d5ab5b1c36af036cbe915d92b90813c384f517a1
giovani-wahl <giovaniwahl@gmail.com>
2024-09-21 08:01:56 -0300

    start

---
+++ investP/src/main/java/com/giovaniwahl/investP/InvestPApplication.java
@@ -0,0 +1,13 @@
+package com.giovaniwahl.investP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestPApplication.class, args);
+	}
+
+}
---
+++ investP/src/test/java/com/giovaniwahl/investP/InvestPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovaniwahl.investP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
